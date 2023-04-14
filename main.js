e9c5251d1553b737c061b588f8ec8457b1bafc14
komyungcheon <shy06101997@gamil.com>
2023-04-14 16:10:51 +0700

    commit

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Stack_1 = require("./Stack");
+var stack = new Stack_1.Stack();
+stack.push(1);
+stack.push(3);
+stack.push(5);
+stack.push(7);
+stack.push(9);
+console.log(stack.size());
---
+++ main.ts
@@ -0,0 +1,8 @@
+import {Stack} from "./Stack";
+let stack = new Stack();
+stack.push(1);
+stack.push(3);
+stack.push(5);
+stack.push(7);
+
+console.log(stack.pop())
